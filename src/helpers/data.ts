f94d37acf4ed5a994f87892417d3b5a600fa63a2
kanalin <kanalin@tencent.com>
2020-05-13 17:20:51 +0800

    feat: 🎸 处理响应 data

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -1,4 +1,5 @@
 /**
+ * request
  * 处理 body 中 data 相关
  * 1. 纯对象
  */
@@ -10,5 +11,20 @@ export function transformRequest(data: any): any {
     return JSON.stringify(data)
   }
 
+  return data
+}
+
+/**
+ * response
+ * 当返回数据是字符串串类型时，尝试转换成 json
+ */
+export function transformResponse(data: any): any {
+  if (typeof data === 'string') {
+    try {
+      data = JSON.parse(data)
+    } catch (e) {
+      // nothing
+    }
+  }
   return data
 }
